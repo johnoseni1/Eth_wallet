d8fa6eee094a8ccad56cc4c6dbbdd265e9cfad6c
John Oseni <johnoseni26@gmail.com>
2022-01-06 15:14:37 +0100

    this is it

--- src/components/App.js
+++ src/components/App.js
@@ -57,14 +57,9 @@ class App extends Component {
     return (
       <div>
         <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-          <a
-            className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="http://www.dappuniversity.com/bootcamp"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Dapp University
-          </a>
+        
+            ETH_wallet
+     
         </nav>
         <div className="container-fluid mt-5">
           <div className="row">
